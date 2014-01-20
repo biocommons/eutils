2ee2b9ba6ec377caa750fe9b3f10b6fd67752ef3
Reece Hart <reecehart@gmail.com>
2014-01-19 17:01:27 -0800

    added type, genus_species, and common_tax methods to gene

--- eutils/xmlfacades/gene.py
+++ eutils/xmlfacades/gene.py
@@ -19,6 +19,10 @@ class Gene(eutils.xmlfacades.base.Base):
         return xml_get_text_or_none(self._xmlroot,'/Entrezgene-Set/Entrezgene/Entrezgene_gene/Gene-ref/Gene-ref_maploc')
 
     @property
+    def references(self):
+        return [ GeneCommentary(gc) for gc in self._xmlroot.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_locus/Gene-commentary') ]
+
+    @property
     def summary(self):
         return xml_get_text_or_none(self._xmlroot,'/Entrezgene-Set/Entrezgene/Entrezgene_summary')
 
@@ -27,8 +31,16 @@ class Gene(eutils.xmlfacades.base.Base):
         return self._xmlroot.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_gene/Gene-ref/Gene-ref_syn/Gene-ref_syn_E/text()')
 
     @property
-    def references(self):
-        return [ GeneCommentary(gc) for gc in self._xmlroot.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_locus/Gene-commentary') ]
+    def type(self):
+        return self._xmlroot.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_type/@value')[0]
+
+    @property
+    def genus_species(self):
+        return self._xmlroot.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_source/BioSource/BioSource_org/Org-ref/Org-ref_taxname/text()')[0]
+
+    @property
+    def common_tax(self):
+        return self._xmlroot.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_source/BioSource/BioSource_org/Org-ref/Org-ref_common/text()')[0]
 
 
     ############################################################################
