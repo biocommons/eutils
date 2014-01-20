74425810682e0e32b86b36a6a44e7886e3bd9fc3
Reece Hart <reece@invitae.com>
2014-01-19 16:37:29 -0800

    overhauled Gene xml facade

--- eutils/xmlfacades/base.py
+++ eutils/xmlfacades/base.py
@@ -5,3 +5,9 @@ class Base(object):
     def __init__(self,xml):
         self._xml = xml
         self._xmlroot = lxml.etree.XML(xml)
+
+    @classmethod
+    def _validate_xml(xml):
+        """validate the xml during initialization. Subclasses override this
+        to apply any __init__-time validation of the incoming XML"""
+        pass
