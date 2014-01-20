647fa42d034725dfbe44ab04c9227d1382254dd2
Reece Hart <reece@invitae.com>
2014-01-19 22:30:37 -0800

    added gbset xmlfacade

--- eutils/utils.py
+++ eutils/utils.py
@@ -16,3 +16,16 @@ def xml_get_text_or_none(node,xpath):
     except IndexError:          # xpath search found 0 matches
         return None
 
+
+def xml_xpath_text(node,xpath):
+    return [ n.text for n in node.xpath(xpath) ]
+def xml_xpath_text_first(node,xpath):
+    try: 
+        return xml_xpath_list_text(node,xpath)[0]
+    except IndexError:
+        return None
+
+
+def arglist_to_dict(**args):
+    return dict(**args)
+a2d = arglist_to_dict
