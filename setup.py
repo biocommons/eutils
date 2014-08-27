55258c13d9e2b8883756d898287351ec72bc2cad
Reece Hart <reece@invitae.com>
2014-08-27 15:36:59 -0700

    update repo URL everywhere for move to https://bitbucket.org/eutils/eutils

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description = """Structured Python interface to NCBI E-Utilities.""",
     name = "eutils",
     packages = find_packages(),
-    url = 'https://bitbucket.org/invitae/eutils',
+    url = 'https://bitbucket.org/eutils/eutils',
 
      classifiers = [
         "Development Status :: 3 - Alpha",
@@ -63,7 +63,7 @@ setup(
 )
 
 ## <LICENSE>
-## Copyright 2014 Eutils Contributors (https://bitbucket.org/invitae/eutils)
+## Copyright 2014 Eutils Contributors (https://bitbucket.org/eutils/eutils)
 ## 
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
