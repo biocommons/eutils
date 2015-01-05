337bfd16cac7a9dd4fe2fe6b8997fdb317701d6b
Naomi Most <naomi.most@invitae.com>
2015-01-05 23:38:31 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def version_handler(mgr, options):
 setup(
     license = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)',
     long_description = long_description,
-    'use_vcs_version = {'version_handler': version_handler},
+    use_vcs_version = {'version_handler': version_handler},
     zip_safe = True,
 
     author = 'Reece Hart',
