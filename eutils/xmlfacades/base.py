318213651b833c43e0446fdd7752a3441fc5e8d8
Reece Hart <reece@invitae.com>
2015-06-04 13:41:56 -0700

    cleaned up GeneCommentary tests

--- eutils/xmlfacades/base.py
+++ eutils/xmlfacades/base.py
@@ -4,22 +4,30 @@ from eutils.exceptions import EutilsError
 
 class Base(object):
 
-    def __init__(self,xml):
-        if isinstance(xml, str):
-            self._xml = xml
-            self._xmlroot = lxml.etree.XML(xml)
-        elif isinstance(xml, lxml.etree._Element):
-            self._xml = None
-            self._xmlroot = xml
-        else:
-            raise RuntimeError("Cannot create object from type "+type(xml).__name__)
+    """
+    Root class for all xmlfacade classes.
 
-        if self._root_tag is None:
-            #TODO: Raise deprecation warning
-            pass
-        elif self._root_tag != self._xmlroot.tag:
-            raise EutilsError("XML for {} object must be a {} element".format(
-                type(self).__name__, self._root_tag))
+    
+    """
+    
+    _root_tag = None
+
+    def __init__(self,xml_elem):
+        if isinstance(xml_elem, str):
+            # Eventually, we'll do this...
+            # raise DeprecationWarning("Initializing instances with strings is deprecated; "
+            #                          "please initialize with lxml _Element objects")
+            self._xml = xml_elem
+            self._xmlroot = lxml.etree.XML(xml_elem)
+        elif isinstance(xml_elem, lxml.etree._Element):
+            self._xml = None
+            self._xmlroot = xml_elem
+        else:
+            raise RuntimeError("Cannot create object from type "+type(xml_elem).__name__)
+
+        if self._root_tag is not None and self._root_tag != self._xmlroot.tag:
+            raise EutilsError("XML for {} object must be a {} element (got {})".format(
+                type(self).__name__, self._root_tag, self._xmlroot.tag))
 
     def __str__(self):
         return unicode(self).encode('utf-8')
