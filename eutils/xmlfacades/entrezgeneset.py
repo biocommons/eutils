bd17a6952ff799296ec87adf2e7c1219466a07ea
Reece Hart <reecehart@gmail.com>
2015-06-04 14:31:13 -0700

    added entrezgenes iterator; updated sbin/eutils-shell

--- eutils/xmlfacades/entrezgeneset.py
+++ eutils/xmlfacades/entrezgeneset.py
@@ -3,6 +3,20 @@ from eutils.xmlfacades.entrezgene import Entrezgene
 
 
 class EntrezgeneSet(Base):
+    """Represents a set of genes as provided with an XML reply from an
+    Entrez Gene query.
+
+    This object supports iteration, like this:
+
+    >> xml = gzip.open(os.path.join("entrezgeneset.xml.gz")).read()
+    >> doc = lxml.etree.XML(xml)
+    >> egs = EntrezgeneSet(doc)
+    >> len(egs.entrezgenes)
+    4
+
+    
+
+    """
 
     _root_tag = 'Entrezgene-Set'
 
@@ -12,6 +26,7 @@ class EntrezgeneSet(Base):
 
     @property
     def entrezgenes(self):
+        # TODO: use cache decorator rather than homebrew
         try:
             return self._entrezgenes
         except AttributeError:
@@ -20,3 +35,6 @@ class EntrezgeneSet(Base):
 
     def _entrezgene_nodes(self):
         return self._xml_elem.iterfind('Entrezgene')
+
+    def __iter__(self):
+        return (eg for eg in self.entrezgenes)
