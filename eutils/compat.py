5b7d678e20bd60c171aeb7540a9fd25c00741751
nthmost <nthmost@users.noreply.github.com>
2015-10-15 23:49:59 -0700

    giving compat.py the standardized eutils treatment

--- eutils/compat.py
+++ eutils/compat.py
@@ -1,16 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 """compat.py
 
 Attempts to quarantine as much python 2 / python 3 compatibility debris as possible.
-
-Available methods:
-
-  * ...
 
 Available py2k/py3k "compatified" imports:
 
   * pickle  (synonymizes py2's cPickle and py3's pickle)
 
 """
+#Available methods (none yet):
+#
+#  * ... (add and explain here)
+#
 
 import six
 
