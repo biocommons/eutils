f43bd5f1cde51e58299454bd0007d14476baccab
cariaso <cariaso@gmail.com>
2016-08-05 01:27:43 +0900

    test case for pmc

--- tests/test_eutils_xmlfacades_pubmedcentralarticle.py
+++ tests/test_eutils_xmlfacades_pubmedcentralarticle.py
@@ -19,15 +19,15 @@ class Test_eutils_xmlfacades_PubmedCentralArticle(unittest.TestCase):
 
     def test_PMC3299399(self):
         pma = self._read_article_xml('efetch.fcgi?db=pmc&id=3299399&rettype=xml.xml')
-
-        assert pma.abstract.startswith('In light of observed changes in connectivity')
-        assert pma.doi == '10.3389/fpsyt.2012.00018'
-        assert pma.pmc is '3299399'
+        assert pma.pmc == '3299399'
         assert pma.pmid == '22416237'
+        assert pma.doi == '10.3389/fpsyt.2012.00018'
         assert pma.title == 'The Effects of Psychosis Risk Variants on Brain Connectivity: A Review'
+        assert 'It is characterized by hallucinations and delusions, reduced emotion and cognitive impairment' in pma.body_text
 
     def test___str__(self):
         'Ensure xmlfacade Base object safely converts to string.'
+        # Although I'm agnostic about what the string representation should be, this is the current one
         pmca = self._read_article_xml('efetch.fcgi?db=pmc&id=3299399&rettype=xml.xml')
         assert 'PubmedCentralArticle(pmc=3299399;pmid=22416237;doi=10.3389/fpsyt.2012.00018;The Effects of Psychosis Risk Variants on Brain Connectivity: A Review)' in str(pmca)
 
