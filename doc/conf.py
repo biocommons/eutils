6803907aa0b498841dd9354791e87792f9dec70a
Reece Hart <reecehart@gmail.com>
2018-05-28 18:29:27 -0700

    [mq]: doc-updates

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'eutils'
-copyright = u'2015, eutils Committers'
+copyright = u'2018, eutils Committers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
